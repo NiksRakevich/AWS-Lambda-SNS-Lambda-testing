571d6ca49e018cb2b0f9021b18e669b6d50ad5e7
Andrey Poleschuk <Andrey_Poleschuk@epam.com>
2019-06-13 14:52:15 +0300

    feat: add lambda addAcc for testing SNS filter policy

--- test-service/handler.js
+++ test-service/handler.js
@@ -20,20 +20,58 @@ module.exports.publisher = async (event) => {
       TopicArn: 'arn:aws:sns:us-east-1:044552001992:test-sns-topic',
       
     }, (err, data) => {
-      console.log( err ? `SNS ERROR! ${err}` : `SNS TRIGGERED! ${data}`);
-    }
+      console.log( err ? `SNS ERROR! ${err}` : `SNS TRIGGERED! ${data}`); 
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'Publisher',
+          input: event,
+          }, null, 2),
+          };
+          }
   )
+
+ 
+};
+
+module.exports.createAcc = (event) => {
+  const Account = dynogels.define('monday-2', {
+    hashKey : 'email',
+   
+    timestamps: true,
+   
+    schema : {
+      email: Joi.string().email(),
+      name: Joi.string(),
+    }
+  });
+  
+  const { email, name } = JSON.parse(event.Records[0].Sns.Message);
+  console.log(`MAIL = ${email}, NAME = ${name}`);
+
+  Account.create(
+    {
+      email,
+      name
+    }, 
+    {
+      overwrite: false
+    }, 
+    (err, acc) => {
+      console.log( err ? `Account create false. ${err}` : `Account create successful! ${acc}`);
+    });
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Publisher',
+      message: 'createAcc',
       input: event,
     }, null, 2),
   };
-};
+}
 
-module.exports.createAcc = (event) => {
+
+module.exports.addAcc = (event) => {
   const Account = dynogels.define('monday-2', {
     hashKey : 'email',
    
